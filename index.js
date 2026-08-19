c65872cad773a747e9c4b923a5d6bb9e7c88e433
HCoreBeat <alederibia@gmail.com>
2026-08-19 04:49:32 -0400

    Update CORS middleware to allow PUT requests

--- index.js
+++ index.js
@@ -951,7 +951,7 @@ app.use(cors({
             callback(new Error("No permitido por CORS"));
         }
     },
-    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
 }));
